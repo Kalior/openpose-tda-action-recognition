f159de0b7cebdce8d5cff923d69dd9a75314d2e7
Joel Gustafsson <me@joelgustafsson.com>
2018-07-10 09:57:14 +0200

    Add argparse to main script for video selection

--- atm_openpose.py
+++ atm_openpose.py
@@ -2,8 +2,14 @@ from tracker import Tracker
 import argparse
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Openpose tracking system.')
+    parser.add_argument('--video', type=str, default="media/video.avi",
+                        help='The video to run tracking on.')
+
+    args = parser.parse_args()
     model_path = '/home/kalior/projects/fujitsu-CoE/openpose/models/'
 
     tracker = Tracker(tf_openpose=True, model_path=model_path)
 
-    tracker.video("media/checkout-2.mp4", only_arms=True)
+    tracker.video(args.video, only_arms=True)
