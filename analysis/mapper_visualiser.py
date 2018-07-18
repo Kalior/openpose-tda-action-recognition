6c75f5c7193f78335a403a92e14d4d50310f800d
Joel Gustafsson <me@joelgustafsson.com>
2018-07-18 16:35:42 +0200

    Add function that produces a video of the scene for a chunk

--- analysis/mapper_visualiser.py
+++ analysis/mapper_visualiser.py
@@ -7,15 +7,16 @@ from tracker import TrackVisualiser, Track, Person
 
 class MapperVisualiser:
 
-    def __init__(self, chunks, chunk_frames, original_chunks, frames_per_chunk, selected_keypoints):
+    def __init__(self, chunks, chunk_frames, original_chunks, frames_per_chunk, selected_keypoints, video):
         self.chunks = chunks
         self.chunk_frames = chunk_frames
         self.original_chunks = original_chunks
         self.frames_per_chunk = frames_per_chunk
         self.selected_keypoints = selected_keypoints
+        self.video = video
 
-    def visualise(self, video, graph, labels):
-        capture = cv2.VideoCapture(video)
+    def visualise(self, graph, labels):
+        capture = cv2.VideoCapture(self.video)
 
         nodes = graph['nodes']
         for name, node in nodes.items():
@@ -90,24 +91,28 @@ class MapperVisualiser:
 
         return track
 
-    def chunk_to_video(self, chunk, out_file, start_frame):
+    def chunk_to_video_pose(self, chunk, out_file, start_frame):
         translated_track = self._chunk_to_track(chunk, start_frame)
 
-        frame_width = 100
-        frame_height = 100
-        fps = 10
-
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-
-        writer = cv2.VideoWriter(out_file, fourcc, fps, (frame_width, frame_height))
+        writer, frame_width, frame_height = self._create_writer(out_file)
         visualiser = TrackVisualiser()
+
         for i in range(self.frames_per_chunk):
             translated_image = self._draw_translated_track(
                 translated_track, i, start_frame, visualiser)
-            translated_image = cv2.resize(translated_image, (100, 100))
+            translated_image = cv2.resize(translated_image, (frame_width, frame_height))
             writer.write(translated_image)
 
         writer.release()
+
+    def _create_writer(self, out_file):
+        frame_width = 100
+        frame_height = 100
+        fps = 10
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+        writer = cv2.VideoWriter(out_file, fourcc, fps, (frame_width, frame_height))
+        return writer, frame_width, frame_height
 
     def _draw_translated_track(self, translated_track, i, start_frame, visualiser):
         blank_image = np.zeros((500, 500, 3), np.uint8)
@@ -115,3 +120,33 @@ class MapperVisualiser:
         visualiser.draw_people([translated_track], blank_image, i + start_frame)
 
         return blank_image
+
+    def chunk_to_video_scene(self, chunk, out_file, start_frame):
+        mean = chunk[~np.all(chunk == 0, axis=2)].mean(axis=0)
+        crop_size = 500
+        start_y = int(mean[0] - crop_size / 2)
+        start_x = int(mean[1] - crop_size / 2)
+
+        capture = cv2.VideoCapture(self.video)
+        capture.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+
+        track = self._chunk_to_track(chunk, start_frame)
+        visualiser = TrackVisualiser()
+
+        writer, frame_width, frame_height = self._create_writer(out_file)
+
+        for i in range(self.frames_per_chunk):
+            sucess, image = capture.read()
+            cropped_image = self._draw_track_scene(
+                track, i, start_frame, visualiser, image, start_x, start_y, crop_size)
+            scaled_image = cv2.resize(cropped_image, (frame_width, frame_height))
+            writer.write(scaled_image)
+
+        writer.release()
+
+    def _draw_track_scene(self, track, i, start_frame, visualiser,
+                          original_image, start_x, start_y, crop_size):
+        visualiser.draw_frame_number(original_image, i + start_frame)
+        visualiser.draw_people([track], original_image, i + start_frame, offset_person=False)
+
+        return original_image[start_x:(start_x + crop_size), start_y:(start_y + crop_size)]
