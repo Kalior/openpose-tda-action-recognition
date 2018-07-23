791b5a558a4c48688914e7f9ca2a67882eb453c6
Joel <me@joelgustafsson.com>
2018-07-23 16:47:30 +0200

    Add function to translate chunks to origin on a keypoint basis

--- analysis/post_processor.py
+++ analysis/post_processor.py
@@ -137,3 +137,19 @@ class PostProcessor:
         mean = chunk[~np.all(chunk == 0, axis=2)].mean(axis=0)
 
         chunk[~np.all(chunk == 0, axis=2)] -= mean
+
+    def translate_chunks_to_origin_by_keypoint(self, chunks):
+        translated_chunks = np.copy(chunks)
+
+        for i, chunk in enumerate(translated_chunks):
+            self._translate_by_keypoint(chunk)
+
+        return translated_chunks
+
+    def _translate_by_keypoint(self, chunk):
+        for i in range(chunk.shape[1]):
+            # Don't take unidentified keypoints into account:
+            keypoints = chunk[:, i][~np.all(chunk[:, i] == 0, axis=1)]
+            if keypoints.shape[0] != 0:
+                keypoint_mean = chunk[:, i][~np.all(chunk[:, i] == 0, axis=1)].mean(axis=0)
+                chunk[:, i][~np.all(chunk[:, i] == 0, axis=1)] -= keypoint_mean
