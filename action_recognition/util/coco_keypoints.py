c917d7da0da6f090c6fbe5e11cce53e9e86e88df
Joel <me@joelgustafsson.com>
2018-08-13 11:52:54 +0200

    Change coco_connections into named values

--- action_recognition/util/coco_keypoints.py
+++ action_recognition/util/coco_keypoints.py
@@ -23,6 +23,17 @@ class COCOKeypoints(Enum):
     Background = 18
 
 coco_connections = [
-    (0, 1), (1, 2), (1, 5), (2, 3), (3, 4), (5, 6), (6, 7),
-    (1, 8), (1, 11), (8, 9), (9, 10), (11, 12), (12, 13)
+    (COCOKeypoints.Nose.value, COCOKeypoints.Neck.value),
+    (COCOKeypoints.Neck.value, COCOKeypoints.RShoulder.value),
+    (COCOKeypoints.Neck.value, COCOKeypoints.LShoulder.value),
+    (COCOKeypoints.Neck.value, COCOKeypoints.RHip.value),
+    (COCOKeypoints.Neck.value, COCOKeypoints.LHip.value),
+    (COCOKeypoints.RShoulder.value, COCOKeypoints.RElbow.value),
+    (COCOKeypoints.RElbow.value, COCOKeypoints.RWrist.value),
+    (COCOKeypoints.LShoulder.value, COCOKeypoints.LElbow.value),
+    (COCOKeypoints.LElbow.value, COCOKeypoints.LWrist.value),
+    (COCOKeypoints.RHip.value, COCOKeypoints.RKnee.value),
+    (COCOKeypoints.RKnee.value, COCOKeypoints.RAnkle.value),
+    (COCOKeypoints.LHip.value, COCOKeypoints.LKnee.value),
+    (COCOKeypoints.LKnee.value, COCOKeypoints.LAnkle.value)
 ]
