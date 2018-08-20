8380aaddd7de7c7e5fc616f9c3134180dfb5c71b
Joel <me@joelgustafsson.com>
2018-08-20 18:02:36 +0200

    Improve output from live_prediction

--- live_prediction.py
+++ live_prediction.py
@@ -3,6 +3,7 @@ from sklearn.externals import joblib
 import copy
 from time import time
 import numpy as np
+import logging
 
 from action_recognition.tracker import Tracker, TrackVisualiser
 from action_recognition.detector import CaffeOpenpose
@@ -17,11 +18,12 @@ def main(args):
     processor = PostProcessor()
     vis = TrackVisualiser()
 
-    print("Classes: {}".format(classifier.classes_))
+    classes = classifier.classes_
+    logging.info("Classes: {}".format(classes))
 
     track_people_start = time()
     for tracks, img, current_frame in tracker.video(args.video, True, True):
-        print("Number of tracks: {}".format(len(tracks)))
+        logging.debug("Number of tracks: {}".format(len(tracks)))
         track_people_time = time() - track_people_start
 
         processor.tracks = [copy.deepcopy(t) for t in tracks]
@@ -29,29 +31,38 @@ def main(args):
         processor.post_process_tracks()
         chunks, _, _ = processor.chunk_tracks(30, 0, 30)
 
-        print("Number of chunks: {}".format(len(chunks)))
+        logging.debug("Number of chunks: {}".format(len(chunks)))
 
         predict_people_start = time()
         if len(chunks) > 0:
             predictions = classifier.predict_proba(chunks)
+
+            logging.info("Predictions: " + ", ".join(
+                ["{}: {:.3f}".format(*get_best_pred(prediction, classes))
+                 for prediction in predictions]))
+
             add_predictions_to_img(
-                chunks, predictions, args.probability_threshold, classifier.classes_, vis, img)
+                chunks, predictions, args.probability_threshold, classes, vis, img)
         predict_people_time = time() - predict_people_start
 
-        print("Predict time: {:.3f}, Track time: {:.3f}".format(
+        logging.debug("Predict time: {:.3f}, Track time: {:.3f}".format(
             predict_people_time, track_people_time))
         track_people_start = time()
 
 
+def get_best_pred(prediction, classes):
+    best_pred_i = np.argmax(prediction)
+    probability = prediction[best_pred_i]
+    class_name = classes[best_pred_i]
+    return class_name, probability
+
+
 def add_predictions_to_img(chunks, predictions, probability_threshold, classes, vis, img):
-    for chunk, prediciton in zip(chunks, predictions):
-        best_pred_i = np.argmax(prediciton)
-        probability = prediciton[best_pred_i]
-        pred_name = classes[best_pred_i]
+    for chunk, prediction in zip(chunks, predictions):
+        class_name, probability = get_best_pred(prediction, classes)
         if probability > probability_threshold:
             position = tuple(chunk[-1, 0, :2].astype(np.int))
-            print(pred_name)
-            vis.draw_text(img, "{}: {:.3f}".format(pred_name, probability), position)
+            vis.draw_text(img, "{}: {:.3f}".format(class_name, probability), position)
 
 
 if __name__ == '__main__':
@@ -65,6 +76,8 @@ if __name__ == '__main__':
                         help='The model path for the caffe implementation.')
     parser.add_argument('--probability-threshold', type=float, default=0.8,
                         help='Threshold for how confident the model should be in each prediction.')
+
+    logging.basicConfig(level=logging.DEBUG)
 
     args = parser.parse_args()
     main(args)
