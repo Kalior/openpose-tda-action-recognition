6e7c2b78a8337ad5c9834f4c5a1442ebb2d7c6a4
Joel <me@joelgustafsson.com>
2018-08-30 17:44:18 +0200

    Remove TranslateByKeypoint as I don't think it adds anything

--- action_recognition/transforms/__init__.py
+++ action_recognition/transforms/__init__.py
@@ -10,7 +10,6 @@ from .flatten_chunks import Flatten
 from .flatten_to_3D import FlattenTo3D
 from .smooth_chunks import SmoothChunks
 from .translate_chunks import TranslateChunks
-from .translate_chunks_by_keypoints import TranslateChunksByKeypoints
 from .persistence import Persistence
 from .speed import Speed
 from .extract_keypoints import ExtractKeypoints
